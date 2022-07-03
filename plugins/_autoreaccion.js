a61ade43d5a616638849bb9cb2f38303d304ea50
BrunoSobrino <90165013+BrunoSobrino@users.noreply.github.com>
2022-07-02 19:46:00 -0500

    🧿 𝚃𝚑𝚎 𝙼𝚢𝚜𝚝𝚒𝚌 - 𝙱𝚘𝚝 🔮

--- plugins/_autoreaccion.js
+++ plugins/_autoreaccion.js
@@ -1,3 +1,4 @@
+/*
 let handler = async (m, { conn }) => {
 let emot = pickRandom(["🎃", "❤", "😘", "😍", "💕", "😎", "🙌", "⭐", "👻", "🔥"])
 conn.sendMessage(m.chat, {
@@ -10,3 +11,4 @@ handler.command = new RegExp
 export default handler
 function pickRandom(list) {
 return list[Math.floor(Math.random() * list.length)]}
+*/
