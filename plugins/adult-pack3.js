6b6dc8e97456cec38765591aaad70b8b91353851
siestaymarin <106639503+siestaymarin@users.noreply.github.com>
2022-07-15 14:34:01 -0600

    Update adult-pack3.js

--- plugins/adult-pack3.js
+++ plugins/adult-pack3.js
@@ -7,6 +7,7 @@ conn.sendButton(m.chat, `_🥵 Pack 3 🥵_`, author, url, [['🔄 𝚂𝙸𝙶�
 handler.help = ['pack3']
 handler.tags = ['internet']
 handler.command = /^(pack3)$/i
+handler.limit = 3
 export default handler
 
 global.packmen = [
